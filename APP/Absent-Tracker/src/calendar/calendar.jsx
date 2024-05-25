7d41351867d4ac6ffadb2911244540a0d3337cd0
Alberto Anunciado Jr <aj.anunciado@gmail.com>
2024-05-25 09:14:56 +0800

    will fix

--- APP/Absent-Tracker/src/calendar/calendar.jsx
+++ APP/Absent-Tracker/src/calendar/calendar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import Modal from 'react-modal';
+import './customCalendar.css';
 
 
 Modal.setAppElement('#root');
